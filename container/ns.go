e1e258504b7b70d3e1cc14343338aae0c9fdec3e
zhhuabj <veryhua2006@gmail.com>
2019-07-20 13:41:34 +0800

    microk8s

--- container/ns.go
+++ container/ns.go
@@ -1,31 +1,31 @@
-// Debug it: go build -gcflags "-N -l namesapce.go && cgdb namespace.go
-// Run it:   go run namespace.go 
+// Debug it: go build -gcflags "-N -l" ns.go && cgdb ./ns
+// Run it:   go run ns.go
 package main
 
 import (
-    "log"
-    "os"
-    "os/exec"
-    "syscall"
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
 )
 
 func main() {
-    cmd := exec.Command("sh")
-    cmd.SysProcAttr = &syscall.SysProcAttr{}
-    //cmd.SysProcAttr.Cloneflags = syscall.CLONE_NEWUSER | syscall.CLONE_NEWNS | syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET
-    cmd.SysProcAttr.Cloneflags = syscall.CLONE_NEWUSER | syscall.CLONE_NEWNS  | syscall.CLONE_NEWPID | syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET
-    // 在宿主机上以一个非root用户(eg: hua's userid=1000)运行创建一个User Namespace，Namespace里将被映射成root用户
-//    cmd.SysProcAttr.Credential = &syscall.Credential{
-//        Uid: 0,
-//        Gid: 0,
-//     }
-//    cmd.SysProcAttr.UidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: 1000, Size: 1}}
-//    cmd.SysProcAttr.GidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: 1000, Size: 1}}
-    cmd.Stdin = os.Stdin
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    if err := cmd.Run(); err != nil {
-        log.Fatal(err)
-    }
-    os.Exit(-1)
+	cmd := exec.Command("sh")
+	cmd.SysProcAttr = &syscall.SysProcAttr{}
+	//cmd.SysProcAttr.Cloneflags = syscall.CLONE_NEWUSER | syscall.CLONE_NEWNS | syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET
+	cmd.SysProcAttr.Cloneflags = syscall.CLONE_NEWUSER | syscall.CLONE_NEWNS | syscall.CLONE_NEWPID | syscall.CLONE_NEWIPC | syscall.CLONE_NEWNET
+	// 在宿主机上以一个非root用户(eg: hua's userid=1000)运行创建一个User Namespace，Namespace里将被映射成root用户
+	//    cmd.SysProcAttr.Credential = &syscall.Credential{
+	//        Uid: 0,
+	//        Gid: 0,
+	//     }
+	//    cmd.SysProcAttr.UidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: 1000, Size: 1}}
+	//    cmd.SysProcAttr.GidMappings = []syscall.SysProcIDMap{{ContainerID: 0, HostID: 1000, Size: 1}}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+	os.Exit(-1)
 }
