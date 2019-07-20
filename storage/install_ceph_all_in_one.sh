14bdd6cb3a5095a8fa839d1dc54482b45c487669
zhhuabj <veryhua2006@gmail.com>
2019-07-20 23:32:05 +0800

    ceph

--- storage/install_ceph_all_in_one.sh
+++ storage/install_ceph_all_in_one.sh
@@ -1,13 +1,12 @@
-#Prepare a machine for test, the name of machine is node1
-#nova boot --key-name mykey --image auto-sync/ubuntu-xenial-16.04-amd64-server-20171116-disk1.img --flavor m1.large --nic net-id=$(neutron net-list |grep 'zhhuabj_admin_net' |awk '{print $2}') node1
-#nova floating-ip-associate node1 10.230.65.123
-#ssh ubuntu@10.230.65.123 -v
-
 # Precondition
-sudo -i
-echo '127.0.0.1       localhost' >> /etc/hosts
-echo '192.168.99.124 node1' >> /etc/hosts
-exit
+local_host="`hostname --fqdn`"
+local_ip=`host $local_host 2>/dev/null | awk '{print $NF}' |head -n 1`
+sudo bash -c 'cat >> /etc/hosts' << EOF
+`echo $local_ip`   `echo $local_host`
+EOF
+sudo sed -i "/$local_host/"d /etc/hosts
+ssh-copy-id -i ~/.ssh/id_rsa.pub $local_ip
+ping -c 1 $local_ip
 
 # Create two disk for test
 sudo mkdir -p /images && sudo chown $(whoami) /images
@@ -24,35 +23,37 @@ sudo mount /dev/ceph-volumes/ceph0 /srv/ceph/osd0
 sudo mount /dev/ceph-volumes/ceph1 /srv/ceph/osd1
 
 # Deploy new monitor node
-sudo update && sudo apt-cache policy ceph-deploy
-sudo apt -y install ceph ceph-deploy
+sudo apt update && sudo apt-cache policy ceph-deploy
+sudo apt install -y ceph ceph-deploy
 mkdir -p ceph-cluster && cd ceph-cluster
-ceph-deploy new node1
+ceph-deploy new $local_host
 echo "osd crush chooseleaf type = 0" >> ceph.conf
 echo "osd pool default size = 1" >> ceph.conf
 echo "osd journal size = 100" >> ceph.conf
 echo "rbd_default_features = 1" >> ceph.conf
 
 # Install ceph packages to other nodes
-#ceph-deploy purgedata node1 && ceph-deploy forgetkeys
-ceph-deploy install node1
+#ceph-deploy purgedata $local_host && ceph-deploy forgetkeys
+ceph-deploy install $local_host
+ceph-conf --name mon.monitor --show-config-value admin_socket
 
 # Generate the keys
-ceph-deploy mon create-initial
+ceph-deploy mon destroy $local_host    #monitor is not yet in quorum
+ceph-deploy --overwrite-conf mon create-initial
 
 # Prepare OSDs
-#ceph-deploy osd prepare node1:sdb1:sdc
-ceph-deploy osd prepare node1:/srv/ceph/osd0
-ceph-deploy osd prepare node1:/srv/ceph/osd1
+#ceph-deploy osd prepare ${local_host}:sdb1:sdc
+ceph-deploy osd prepare ${local_host}:/srv/ceph/osd0
+ceph-deploy osd prepare ${local_host}:/srv/ceph/osd1
 
 # Active OSDs
 sudo chmod 777 /srv/ceph/osd0
 sudo chmod 777 /srv/ceph/osd1
-sudo ceph-deploy osd activate node1:/srv/ceph/osd0
-sudo ceph-deploy osd activate node1:/srv/ceph/osd1
+sudo ceph-deploy osd activate ${local_host}:/srv/ceph/osd0
+sudo ceph-deploy osd activate ${local_host}:/srv/ceph/osd1
 
 # Copy the keys to other nodes
-ceph-deploy admin node1
+ceph-deploy admin $local_host
 sudo chmod +r /etc/ceph/ceph.client.admin.keyring
 
 # Verify
@@ -65,8 +66,8 @@ sudo rados mkpool data
 sudo ceph osd pool set data min_size 1 
 sudo rbd create --size 1 -p data test1
 sudo rados -p data ls
-#sudo rbd unmap /dev/rbd0 /dev/null 2>&1
 sudo rbd map test1 --pool data
 rbd showmapped
 sudo mkfs.ext4 /dev/rbd0
 mkdir test && sudo mount /dev/rbd0 test/
+sudo umount test && sudo rbd unmap /dev/rbd0 /dev/null 2>&1
